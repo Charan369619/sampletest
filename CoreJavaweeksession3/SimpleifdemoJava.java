bc7be1d326a91a5c483cf6d3091009c973cf432d
sai charan <mangipudisaicharan1993@gmail.com>
2024-06-19 19:42:26 +0530

    added SimpleifdemoJava code

---
+++ CoreJavaweeksession3/SimpleifdemoJava.java
@@ -0,0 +1,20 @@
+package Corejavaweeksession3;
+
+import java.util.Scanner;
+
+public class SimpleifdemoJava {
+
+	public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+ 
+	}}
+	
+  
+  
+  
+  
+  
+
+	
+
+
