30d8c88bd051f034679ab1924e4a9d78844415d2
sai charan <mangipudisaicharan1993@gmail.com>
2024-06-19 19:49:14 +0530

    added Sumofdigits.java code

---
+++ CoreJavaweeksession3/Sumofdigits.java
@@ -0,0 +1,20 @@
+package Corejavaweeksession3;
+
+public class Sumofdigits {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i=1; i<=30; i+=2) {
+	
+		System.out.println("print all odd numbers up to  30");
+		System.out.println(i);}}}
+				
+			
+			
+			
+		
+
+
+	
+
